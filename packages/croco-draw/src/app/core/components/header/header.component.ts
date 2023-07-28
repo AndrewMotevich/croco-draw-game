3a509603375501fa3285d8ef065693ba0a97b256
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-07-28 22:24:40 +0300

    feat: add styles for header and footer

--- packages/croco-draw/src/app/core/components/header/header.component.ts
+++ packages/croco-draw/src/app/core/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageModule } from 'primeng/image';
 import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'croco-header',
   standalone: true,
-  imports: [CommonModule, ImageModule],
+  imports: [CommonModule, ImageModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
